821b1cc468deded8ef2f1e824cbf98918bb13733
Deepak <dgusaiwal537@Gmail.com>
2023-03-09 11:58:15 +0530

    learn all super globals

--- 10superGlobalsInphp.php
+++ 10superGlobalsInphp.php
@@ -40,5 +40,37 @@
     echo "<br>" . $_SERVER['SERVER_NAME'] . "<br>"; 
     echo "<br>" . $_SERVER['HTTP_HOST'] . "<br>"; 
     ?>
+
+    <!-- Creating a Form for the Use OF $_REQUEST Variable of PHP -->
+    <!-- ===================================================3. $_REQUEST Variable || $_POST || -->
+
+
+    <form method="post" action="10superGlobalsInphp.php" >
+        <input type="text" name="userName" id="" placeholder="Name">
+        <input type="submit">
+    </form>
+    
+    <?php 
+    if($_SERVER['REQUEST_METHOD']=="POST"){
+        // $name = $_REQUEST['userName']; // way 1 to get form data
+        $name = $_POST['userName']; // only work in post method forms
+        if(empty($name)){
+            echo "<h3>Name is Empty</h3>";
+        }else{
+            echo "<h3>User Name is : $name </h3>";
+        }
+    }
+    ?>
+
+
+<!-- ===================================================3. $_GET -->
+<!-- this link will lead us to the textGet.php page in browser -->
+<!-- <a href="10testGet.php"> Get PHP Data</a> -->
+
+<!-- in this way we can send variables and thier values to the textGet.php -->
+<!-- name is a parameter & current is also a parameter -->
+<!-- we can also see their values at url of the 10textGet.php -->
+<a href="10testGet.php?name=Deep&current=Learning PHP"> Get PHP Data</a>
+   
 </body>
 </html>
---
+++ 10testGet.php
@@ -0,0 +1,10 @@
+<?php 
+echo "<h2> This is Text PHP for Get Request by 10superGlobalsphp.php</h2>"
+?>
+
+<!-- here we get the data from 10superGloblsphp.php -->
+<?php
+echo "my name is ".$_GET['name'];
+echo "<br>";
+echo "and currently ".$_GET['current'];
+?>
