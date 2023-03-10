548d4863d7b31ddb005cae81b3f965a89170e330
Deepak <dgusaiwal537@Gmail.com>
2023-03-10 20:51:52 +0530

    learn how to read data of any file using php

---
+++ PHP Advance/1 Date.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dates in PHP</title>
+</head>
+<body>
+    <h2>Dates in PHP</h2>
+    <?php 
+    echo '<h2>Today is :   '.date("Y/m/d").'  </h2>  <br>';
+    echo "Today is " . date("l"); // l will gives day
+    ?>
+</body>
+</html>
